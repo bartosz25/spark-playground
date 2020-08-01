bfd711188f3604a029e1974f5530e6b3df100150
bartosz25 <bartkonieczny@yahoo.fr>
2020-08-01 18:45:54 +0200

    add demo for broadcast & join in Structured Streaming

---
+++ broadcast-join-structured-streaming/src/main/scala/com/waitingforcode/BroadcastJoinDemo.scala
@@ -0,0 +1,74 @@
+package com.waitingforcode
+
+import java.io.File
+
+import org.apache.commons.io.FileUtils
+import org.apache.spark.sql.SparkSession
+
+object TestConfiguration {
+  val datasetPath = "/tmp/wfc/broadcast-join/test_dataset"
+}
+
+// Run DataOverwrite to generate the first version of the files
+
+// Later, setup Kafka context:
+// docker exec -ti broker_kafka_1 bin/bash
+// kafka-topics.sh --bootstrap-server localhost:9092 --topic broadcast_join_demo --delete
+
+// Next, create with only 2 partitions for easier debugging
+// kafka-topics.sh --bootstrap-server localhost:9092 --topic broadcast_join_demo --partitions 2 --create
+// kafka-console-producer.sh --broker-list localhost:29092 --topic broadcast_join_demo
+object BroadcastJoinDemo extends App {
+
+  val sparkSession = SparkSession.builder()
+    .appName("Broadcast join in Structured Streaming").master("local[*]")
+    // set a small number for the shuffle partitions to avoid too many debug windows opened
+    // (shuffle will happen during the join)
+    .config("spark.sql.shuffle.partitions", 1)
+    .getOrCreate()
+  import sparkSession.implicits._
+
+  val timestampsDataset = sparkSession.read.json(TestConfiguration.datasetPath)
+
+  val kafkaSource = sparkSession.readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:29092")
+    .option("client.id", "broadcast_join_client")
+    .option("subscribe", "broadcast_join_demo")
+    .option("startingOffsets", "EARLIEST")
+    .load()
+
+  val writeQuery = kafkaSource.selectExpr("CAST(value AS STRING) AS value_key")
+    .join(timestampsDataset, $"value_key" === $"nr", "left_outer")
+    .writeStream
+    .format("console")
+    .option("truncate", false)
+
+  val startedQuery = writeQuery.start()
+  startedQuery.awaitTermination()
+
+}
+
+object DataOverwriter extends App {
+
+  val version1 = s"""
+                    |{"nr": 0, "is_even": true, "ts": "${System.currentTimeMillis()}"}
+                    |{"nr": 2, "is_even": true, "ts": "${System.currentTimeMillis()}"}
+                    |{"nr": 4, "is_even": true, "ts": "${System.currentTimeMillis()}"}
+                    |{"nr": 6, "is_even": true, "ts": "${System.currentTimeMillis()}"}
+                    |""".stripMargin
+  val version2 = s"""
+                    |{"nr": 0, "is_even": true, "new_field": "a", "ts": "${System.currentTimeMillis()}"}
+                    |{"nr": 2, "is_even": true, "new_field": "a","ts": "${System.currentTimeMillis()}"}
+                    |{"nr": 4, "is_even": true, "new_field": "a","ts": "${System.currentTimeMillis()}"}
+                    |{"nr": 6, "is_even": true, "new_field": "a","ts": "${System.currentTimeMillis()}"}
+                    |""".stripMargin
+
+  val versionToWrite = version2
+
+  // To illustrate that the new dataset is taken, hence the batch dataset is read,
+  // replace the `versionToWrite` by `version2` and rerun the generator
+
+  FileUtils.deleteDirectory(new File(TestConfiguration.datasetPath))
+  FileUtils.writeStringToFile(new File(s"${TestConfiguration.datasetPath}/test.json"), versionToWrite)
+}
---
+++ broadcast-join-structured-streaming/src/main/scala/com/waitingforcode/BroadcastVariableDemo.scala
@@ -0,0 +1,55 @@
+package com.waitingforcode
+
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.sql.{Row, SparkSession}
+
+
+// Steps to run the demo:
+// docker exec -ti broker_kafka_1 bin/bash
+// kafka-topics.sh --bootstrap-server localhost:9092 --topic broadcast_variable_demo --delete
+
+// Next, create with only 2 partitions for easier debugging
+// kafka-topics.sh --bootstrap-server localhost:9092 --topic broadcast_variable_demo --partitions 2 --create
+// kafka-console-producer.sh --broker-list localhost:29092 --topic broadcast_variable_demo
+object BroadcastVariableDemo extends App {
+
+  val sparkSession = SparkSession.builder()
+    .appName("Broadcast variable demo in Structured Streaming").master("local[*]")
+    .getOrCreate()
+  import sparkSession.implicits._
+
+  // I'm running this process to change the variable and see whether
+  // it's broadcasted to the micro-batch query
+  var commonLabel = "abc -> "
+  new Thread(() => {
+    while (true) {
+      commonLabel += "."
+      println(s"Common label after change=${commonLabel}")
+      Thread.sleep(5000)
+    }
+  }).start()
+  val commonLabelBroadcast = sparkSession.sparkContext.broadcast(commonLabel)
+
+  val kafkaSource = sparkSession.readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:29092")
+    .option("client.id", "broadcast_variable_client")
+    .option("subscribe", "broadcast_variable_demo")
+    .option("startingOffsets", "EARLIEST")
+    .load()
+
+  def decorateValueKeyWithLabel(row: Row, label: Broadcast[String]): String = {
+    val valueKey = row.getAs[String]("value_key")
+    s"${label.value}${valueKey}"
+  }
+
+  val writeQuery = kafkaSource.selectExpr("CAST(value AS STRING) AS value_key")
+    .map(row => decorateValueKeyWithLabel(row, commonLabelBroadcast))
+    .writeStream
+    .format("console")
+    .option("truncate", false)
+
+  val startedQuery = writeQuery.start()
+  startedQuery.awaitTermination()
+
+}
