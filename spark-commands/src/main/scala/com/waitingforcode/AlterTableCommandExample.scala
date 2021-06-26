3116e8e44df403034ec5c261741df73c7b634b56
bartosz25 <bartkonieczny@yahoo.fr>
2021-06-26 17:57:52 +0200

    add some missing code to the Commands demo

--- spark-commands/src/main/scala/com/waitingforcode/AlterTableCommandExample.scala
+++ spark-commands/src/main/scala/com/waitingforcode/AlterTableCommandExample.scala
@@ -8,7 +8,7 @@ object AlterTableCommandExample extends App {
     .appName("Alter table example").master("local[*]")
     .enableHiveSupport()
     .getOrCreate()
-  // How many of these are commands? All except the saveAsTable!
+  // How many of these are commands? All except the saveAsTable and outputShowTables.write!
 
   // DropTableCommand
   sparkSession.sql("DROP TABLE users_list")
@@ -20,15 +20,23 @@ object AlterTableCommandExample extends App {
   // AlterTableAddColumnsCommand#1
   val outputAlterTable = sparkSession.sql("ALTER TABLE users_list ADD COLUMNS first_name STRING")
   outputAlterTable.show(false)
+  outputAlterTable.show(false)
+  outputAlterTable.show(false)
+  outputAlterTable.show(false)
+  outputAlterTable.show(false)
 
   // AlterTableAddColumnsCommand#1
   // The lazy side effects field would work for the sql(...) + show(...) but
   // not for rerunning the same command!
   // Uncomment and rerun to see this!
-  // sparkSession.sql("ALTER TABLE users_list ADD COLUMNS first_name STRING")
+  sparkSession.sql("ALTER TABLE users_list ADD COLUMNS first_name STRING")
 
   // ShowTablesCommand
   // Example to show the output generation, missing in the ALTER or DROP
   val outputShowTables = sparkSession.sql("SHOW TABLES")
+  // Prove that SHOW TABLES is not called 3 times here!
   outputShowTables.show(false)
+  outputShowTables.write.csv(s"/tmp/test${System.currentTimeMillis()}")
+
+  Thread.sleep(200000)
 }
