fd61b9c4ef00b8559493d344403e92fbf0c44287
bartosz25 <bartkonieczny@yahoo.fr>
2022-10-11 04:52:12 +0200

    remove irrelevant code for the demo

--- pyspark-vectorized-udf/pyspark_vectorized_udf.py
+++ pyspark-vectorized-udf/pyspark_vectorized_udf.py
@@ -15,20 +15,8 @@ def multiply(numbers_vector: pandas.Series) -> pandas.Series:
     return numbers_vector.apply(lambda nr: nr * 2)
 
 
-
 numbers_with_results = numbers.withColumn("result", multiply(col("id")))
 
 numbers_with_results.show(truncate=False)
 
 numbers_with_results.explain(extended=True)
-
-@pandas_udf("col1 string, col2 long")
-def func(s1: pandas.Series, s2: pandas.Series, s3: pandas.DataFrame) -> pandas.DataFrame:
-     s3['col2'] = s1 + s2.str.len()
-     return s3
-
-# Create a Spark DataFrame that has three columns including a struct column.
-df = spark.createDataFrame([[1, "a string", ("a nested string",)]], "long_col long, string_col string, struct_col struct<col1:string>")
-df.printSchema()
-
-df.select(func("long_col", "string_col", "struct_col")).explain(extended=True)
