d3f10e76eadec1c57a1800e6dcf48dcfd478fe32
bartosz25 <bartkonieczny@yahoo.fr>
2023-05-11 08:23:16 +0200

    two other new features of Structured Streaming 3.4.0

---
+++ spark-3.4.0-features/structured_streaming/src/main/scala/com/waitingforcode/ProtobufSupport.scala
@@ -0,0 +1,53 @@
+package com.waitingforcode
+
+import org.apache.commons.io.FileUtils
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.struct
+import org.apache.spark.sql.protobuf.functions.{from_protobuf, to_protobuf}
+
+import java.io.File
+import java.util.UUID
+
+object ProtobufSupport {
+
+  def main(args: Array[String]): Unit = {
+    val mainPath = "/tmp/spark/3.4.0/protobuf"
+    val sparkSession = SparkSession.builder().master("local[*]")
+      .config("spark.sql.shuffle.partitions", 1)
+      .getOrCreate()
+
+    val protoSchema =
+      """syntax  = "proto3";
+        |
+        |message TestEvent {
+        |     string id = 1;
+        |     string content = 2;
+        |}""".stripMargin
+    val schemaPath = s"${mainPath}/protobuf.schema"
+    FileUtils.writeStringToFile(new File(schemaPath), protoSchema, "UTF-8")
+
+    val descriptorPath = s"${mainPath}/protobuf.pb"
+    // To generate the descriptor file, you must install the `protoc` library
+    // and call:
+    // protoc --descriptor_set_out=/tmp/spark/3.4.0/protobuf/protobuf.pb --proto_path=/tmp/spark/3.4.0/protobuf protobuf.schema
+
+    import sparkSession.implicits._
+    val datasetWithProtobuf = Seq(
+      (UUID.randomUUID().toString, """{some event data}"""),
+      (UUID.randomUUID().toString, """{other event data}""")
+    ).toDF("id", "content")
+      .select(to_protobuf(
+        data = struct($"id", $"content"),
+        messageName = "TestEvent",
+        descFilePath = descriptorPath).as("value"))
+    datasetWithProtobuf.show(false)
+
+    datasetWithProtobuf.select(
+      from_protobuf(
+        data = $"value",
+        messageName = "TestEvent",
+        descFilePath = descriptorPath
+      )
+    ).show(false)
+  }
+}
