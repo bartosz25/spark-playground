2058e97c7bd8327f4206fe6da548caeea4f3b44a
bartosz25 <bartkonieczny@yahoo.fr>
2023-07-13 06:35:30 +0200

    add demo for the _spark_metadata

---
+++ structured-streaming-spark-metadata/src/main/scala/com/becomedataengineer/SparkMetadataIssueGenerator.scala
@@ -0,0 +1,36 @@
+package com.becomedataengineer
+
+import org.apache.spark.sql.SparkSession
+
+import java.sql.Timestamp
+import java.util.TimeZone
+
+object SparkMetadataIssueGenerator {
+
+  def main(args: Array[String]): Unit = {
+    val sparkSession = SparkSession.builder().master("local[2]")
+      .config("spark.sql.shuffle.partitions", 2)
+      .config("spark.sql.session.timeZone", "UTC")
+      .config("spark.sql.streaming.minBatchesToRetain", 10)
+      .getOrCreate()
+    import sparkSession.implicits._
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    val startTimestamp = Timestamp.valueOf("2023-07-01 09:00:00")
+    val inputStream = sparkSession.readStream.format("rate-micro-batch")
+      // To better simulate the issue, let's define a lot of partitions
+      .option("numPartitions", 15)
+      .option("rowsPerBatch", 30)
+      .option("advanceMillisPerBatch", 60000)
+      .option("startTimestamp", startTimestamp.getTime)
+      .load()
+
+    val writeQuery = inputStream
+      .writeStream
+      .format("json")
+      .option("checkpointLocation",  "/tmp/wfc/spark_metadata_issue/checkpoint")
+      .option("path", "/tmp/wfc/spark_metadata_issue/data")
+      .start()
+
+    writeQuery.awaitTermination()
+  }
+}
